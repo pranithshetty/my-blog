7004ddb585fe44e80009fca10a8483af3ba5607d
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-15 13:38:52 +0530

    Redux store + components + tailwinf setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,37 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { useEffect } from "react";
+import { login, logout } from "./store/authSlice";
+import { Header, Footer } from "./components";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
+  const [isLoading, setIsLoading] = useState(true);
+  const dispatch = useDispatch("auth");
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
 
-  return (
-    <>
-      
-    </>
-  )
+  return !isLoading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-green-50">
+      <div className="w-full block">
+        <Header />
+        <main>{/* <Outlet/> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
