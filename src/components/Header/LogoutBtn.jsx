b9054ad2198e77cbe67eae605f3baf5ebb859e70
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-19 14:47:31 +0530

    typo fix

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -15,7 +15,7 @@ const LogoutBtn = () => {
       className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
       onClick={logOutHandler}
     >
-      LogoutBtn
+      Logout
     </button>
   );
 };
