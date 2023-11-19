367ee56785848b8d9328e4e11bed5313346f12f5
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-19 13:52:45 +0530

    fixed link path

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -3,7 +3,7 @@ import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
 const PostCard = ({ $id, title, featuredImage }) => {
   return (
-    <Link to={`post/${$id}`}>
+    <Link to={`/post/${$id}`}>
       <div className="w-full bg-gray-100 rounded-xl p-4">
         <div className="w-full justify-center mb-4">
           <img
