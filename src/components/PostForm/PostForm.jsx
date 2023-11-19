754fcf7104970d2cd470c0e2e08970f433d90aee
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-19 10:56:01 +0530

    Regex error:fixed

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -1,6 +1,6 @@
 import React, { useCallback } from "react";
 import { useForm } from "react-hook-form";
-import { Button, Input, RichTextEditior } from "../index";
+import { Button, Input, RichTextEditior, Select } from "../index";
 import appwriteService from "../../appwrite/config";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -46,15 +46,16 @@ const PostForm = ({ post }) => {
     }
   };
 
-  const slugTransform = (value) => {
-    if (value && typeof value === "string") {
+  const slugTransform = useCallback((value) => {
+    if (value && typeof value === "string")
       return value
         .trim()
         .toLowerCase()
-        .replace(/^[a-zA-Z\d\s]+/g, "-")
+        .replace(/[^a-zA-Z\d\s]+/g, "-")
         .replace(/\s/g, "-");
-    }
-  };
+
+    return "";
+  }, []);
 
   useEffect(() => {
     const subscription = watch((value, { name }) => {
