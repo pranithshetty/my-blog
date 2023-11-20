ff7b21701e9eab6024bb47c3779c671288003353
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-20 17:42:42 +0530

    remove lazy load for post

--- src/main.jsx
+++ src/main.jsx
@@ -11,14 +11,14 @@ import Home from "./Pages/Home.jsx";
 //import AddPost from "./Pages/AddPost";
 // import EditPosts from "./Pages/EditPosts";
 // import AllPosts from "./Pages/AllPosts";
-//import Post from "./Pages/Post";
+import Post from "./Pages/Post";
 import PageNotFound from "./components/PageNotFound.jsx";
 import Skeleton from "./components/Skeleton.jsx";
 
 const AddPost = lazy(() => import("./Pages/AddPost"));
 const EditPosts = lazy(() => import("./Pages/EditPosts"));
 const AllPosts = lazy(() => import("./Pages/AllPosts"));
-const Post = lazy(() => import("./Pages/Post"));
+//const Post = lazy(() => import("./Pages/Post"));
 
 const router = createBrowserRouter([
   {
@@ -81,11 +81,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/post/:slug",
-        element: (
-          <Suspense fallback={<Skeleton />}>
-            <Post />
-          </Suspense>
-        ),
+        element: <Post />,
       },
     ],
   },
