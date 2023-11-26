0481fb87cab7edaf0557ec3e15f66cf85e186f36
pranithshetty <shetty.praneeth4@gmail.com>
2023-11-26 18:54:22 +0530

    fixes

--- src/Pages/Post.jsx
+++ src/Pages/Post.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import appwriteService from "../appWrite/Config";
-import { Button, Container } from "../Components";
+import appwriteService from "../appWrite/config";
+import { Button, Container } from "../components";
 import parse from "html-react-parser";
 import { useSelector } from "react-redux";
 
